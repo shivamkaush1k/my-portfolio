7ba2248a2f41d5e7dc1b8932106fc884dd0f5e7c
shivamkaush1k <shivamkaushik.1004@gmail.com>
2026-03-26 18:19:09 +0530

    updted settings for deployment

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = [
     host.strip()
-    for host in os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost","sh1vam-portfolio-rbad0yerx-shivamkaush1ks-projects.vercel.app").split(",")
+    for host in os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost,.vercel.app").split(",")
     if host.strip()
 ]
 

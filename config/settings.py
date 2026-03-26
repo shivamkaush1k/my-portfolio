58b3bce463606324834ad8859ef3ba42d1618479
shivamkaush1k <shivamkaushik.1004@gmail.com>
2026-03-26 19:18:43 +0530

    Static files updated.

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,6 @@ ALLOWED_HOSTS = [
     host.strip()
     for host in os.getenv(
         "ALLOWED_HOSTS",
-        "127.0.0.1,localhost,.vercel.app",
         "https://sh1vam-portfolio-d2ihubvda-shivamkaush1ks-projects.vercel.app/")
         .split(",")
     if host.strip()

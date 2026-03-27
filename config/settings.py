7db6e42645ed11a540b2bfbb14b0e7765b755759
shivamkaush1k <shivamkaushik.1004@gmail.com>
2026-03-27 22:24:54 +0530

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -93,11 +93,8 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 

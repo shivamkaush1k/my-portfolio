a5efad8161509f0d81781fab74b6972623be8b2e
shivamkaush1k <shivamkaushik.1004@gmail.com>
2026-03-27 22:57:30 +0530

    Fix STORAGES default backend for ImageField

--- config/settings.py
+++ config/settings.py
@@ -93,8 +93,11 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
